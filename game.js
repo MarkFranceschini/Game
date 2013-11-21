8db1c7e7ded9dc3d6957b0225238861506721ce9
Michael Pair <michael.pair1@marist.edu>
2013-11-21 09:16:44 -0500

    riddle select and game start button
    I made it so that each room can select a riddle at random and I also
    got the start game button working, when you click it, the button
    dissappears and the room buttons show up :D

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+function start() {
+    document.getElementById('roomStart').style.display = "none";
+}
+
 function username(){
     userName=document.getElementById('username').value;
     return "Hello " + userName + ", welcome the the dungeon! Muahahahahaha";
@@ -8,10 +12,29 @@ function startGame() {
     document.getElementById('roomStart').style.display = "block";
 }
 
+function chosenriddle() {
+    var riddle = RandomOneOf(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+    return riddle;
+}
 
 function riddleChooser() {
-    var chosenRiddle = RandomOneOf(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-        return document.getElementById('riddle').innerHTML = chosenRiddle;
+        return document.getElementById('riddle').innerHTML = chosenriddle();
+}
+
+function riddleChooser2() {
+        return document.getElementById('riddle2').innerHTML = chosenriddle();
+}
+
+function riddleChooser3() {
+        return document.getElementById('riddle3').innerHTML = chosenriddle();
+}
+
+function riddleChooser4() {
+    return document.getElementById('riddle4').innerHTML = chosenriddle();
+}
+
+function riddleChooser5() {
+    return document.getElementById('riddle5').innerHTML = chosenriddle();
 }
 
 function RandomNum(low, high) 
@@ -32,3 +55,14 @@ function RandomOneOf(list)
 {    
     return list[RandomInt(0, list.length-1)]; 
 }
+
+function nextPage(curr) {
+      var next, currPage, nextPage;
+
+      next = curr + 1;
+      currPage = document.getElementById('page' + curr);
+      nextPage = document.getElementById('page' + next);
+      
+      currPage.style.display = 'none';
+      nextPage.style.display = 'block';
+    }
